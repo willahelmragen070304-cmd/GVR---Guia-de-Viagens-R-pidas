f7831572ab0a089b27feaf5c8ae8ecc6ed116f12
willahelmragen070304-cmd <willahelmragen070304@gmail.com>
2025-11-25 21:13:42 -0300

    Update database.js

--- Projeto-GVR-PW/js/database.js
+++ Projeto-GVR-PW/js/database.js
@@ -1,1 +1,13 @@
+function getDestinos() {
+    return JSON.parse(localStorage.getItem("destinos")) || [];
+}
 
+function saveDestinos(lista) {
+    localStorage.setItem("destinos", JSON.stringify(lista));
+}
+function adicionarDestino(destino) {
+    const lista = getDestinos();
+    destino.id = Date.now(); // gera ID único
+    lista.push(destino);
+    saveDestinos(lista);
+}
