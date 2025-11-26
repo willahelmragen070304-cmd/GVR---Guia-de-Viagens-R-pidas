0aba839d3eece16e53bb95b1af9bcb24646e6b4f
willahelmragen070304-cmd <willahelmragen070304@gmail.com>
2025-11-26 12:31:53 -0300

    Update destinos.js

--- Projeto-GVR-PW/js/destinos.js
+++ Projeto-GVR-PW/js/destinos.js
@@ -1,13 +1,18 @@
-function getDestinos() {
-    return JSON.parse(localStorage.getItem("destinos")) || [];
-}
-
-function saveDestinos(lista) {
-    localStorage.setItem("destinos", JSON.stringify(lista));
-}
-function adicionarDestino(destino) {
-    const lista = getDestinos();
-    destino.id = Date.now(); // gera ID único
-    lista.push(destino);
-    saveDestinos(lista);
-}
+[
+    {
+        "nome": "Chile",
+        "continente": "América do Sul",
+        "tipo": "Cidade",
+        "preco": 2500,
+        "descricao": "Viagem para Santiago com museus, vinícolas e paisagens andinas.",
+        "imagem": "../imagens/card1.png"
+    },
+    {
+        "nome": "Portugal",
+        "continente": "Europa",
+        "tipo": "Cidade",
+        "preco": 3800,
+        "descricao": "Lisboa histórica com gastronomia e praças famosas.",
+        "imagem": "../imagens/card2.png"
+    }
+]
